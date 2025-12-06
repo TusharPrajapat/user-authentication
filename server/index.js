eec16e78891b26b1a84ced480fb58dee8e22025b
TusharPrajapat <tusharprajapat41@gmail.com>
2025-12-06 11:15:56 +0530

    Initial commit with basic client and server set-up

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express(); //Creating express server
+require("dotenv").config();
+const { MONGO_URL, PORT } = process.env;
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected To DB!");
+  })
+  .catch((err) => {
+    console.log("Server is NOT CONNECTED TO DB!", err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
