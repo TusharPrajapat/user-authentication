a667d58606b8b75de692f044bf8e2e29e48822b2
TusharPrajapat <tusharprajapat41@gmail.com>
2025-12-07 21:59:24 +0530

    Setup Login Route

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -26,3 +26,45 @@ module.exports.Signup = async (req, res, next) => {
     console.error(error);
   }
 };
+
+module.exports.Login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    //1. Check required fields
+    if (!email || !password) {
+      return res.json({ message: "All fields are required" });
+    }
+
+    //2. Find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+
+    //3. Check password
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+
+    //4. Create token
+    const token = createSecretToken(user._id);
+
+    //5. Set token inside cookie
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+
+    //6. Send response
+    res.status(201).json({
+      message: "User logged in successfully",
+      success: true,
+    });
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error, please try again" });
+  }
+};
